7d47f1812aaa6c3726a8f52d13f229367d57b191
shreyamagar16 <shreyamagar0204@gmail.com>
2026-07-07 17:54:23 +0530

    Implemented MAP@3 evaluation metric

---
+++ train.py
@@ -0,0 +1,15 @@
+from utils import map_at_3
+
+if __name__ == "__main__":
+
+    actual = ["A", "C", "B"]
+
+    predictions = [
+        ["A", "C", "D"],
+        ["B", "C", "A"],
+        ["B", "D", "E"]
+    ]
+
+    score = map_at_3(actual, predictions)
+
+    print(f"Validation MAP@3 : {score:.4f}")
--- utils/utils.py
+++ utils/utils.py
@@ -16,3 +16,24 @@ def tokenize(text):
 if __name__ == "__main__":
     sample = "Hello, NLP World!"
     print(tokenize(sample))
+
+
+def average_precision_at_3(actual, predicted):
+    """
+    Compute Average Precision@3 for a single prediction.
+    """
+    if actual in predicted[:3]:
+        return 1.0 / (predicted.index(actual) + 1)
+    return 0.0
+
+
+def map_at_3(actual_labels, predicted_labels):
+    """
+    Compute Mean Average Precision@3.
+    """
+    scores = []
+
+    for actual, predicted in zip(actual_labels, predicted_labels):
+        scores.append(average_precision_at_3(actual, predicted))
+
+    return sum(scores) / len(scores)
